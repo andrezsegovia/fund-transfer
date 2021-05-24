c91ea3429b25da45189f0cfb661d27801d4ea949
Andrez S <ajsegoviacruz93@gmail.com>
2021-05-23 20:33:33 -0500

    WIP: Implementing transfer funds functionality

---
+++ transfer-service/src/test/java/com/yellowpepper/transferservice/integrationTests/stepsDef/TransferFunds.java
@@ -0,0 +1,49 @@
+package com.yellowpepper.transferservice.integrationTests.stepsDef;
+
+import com.yellowpepper.transferservice.integrationTests.IntegrationTests;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TransferFunds extends IntegrationTests {
+    @Given("a client with account number {int}")
+    public void a_client_with_account_number(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("a funds amount of {int} USD")
+    public void a_funds_amount_of_usd(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("wants to make fund transfer of {int} USD")
+    public void wants_to_make_fund_transfer_of_usd(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("to the account number {int}")
+    public void to_the_account_number(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("with the description")
+    public void with_the_description(String docString) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("makes a POST call to {string}")
+    public void makes_a_post_call_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("transfer should be success with a {int} HTTP Status Code")
+    public void transfer_should_be_success_with_a_http_status_code(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("returns a JSON response")
+    public void returns_a_json_response(String docString) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- transfer-service/src/test/resources/features/TransferFunds.feature
+++ transfer-service/src/test/resources/features/TransferFunds.feature
@@ -1,9 +1,3 @@
-
-Feature: Transfer Controller should return 200 when is call
-  Scenario: client makes a GET request to transfer endpoint
-    When the client call to "/"
-    Then the client receives a 200 HTTP Status Code
-
 Feature: Client can transfer funds
 
   A client should be able to transfer funds from his/her account to another account. Hi/She can only make three
@@ -11,6 +5,25 @@ Feature: Client can transfer funds
   the client. If the client exceeds the three transfer per day should be informed and the transfer should be cancel.
 
   Scenario: Client performs a fund transfer successfully
+    Given a client with account number 12345600
+    And a funds amount of 1000 USD
+    When wants to make fund transfer of 500 USD
+    And to the account number 12345601
+    And with the description
+      """
+      Hey dude! I am sending you the money you loaned to me last week.
+      """
+    And makes a POST call to "/"
+    Then transfer should be success with a 200 HTTP Status Code
+    And returns a JSON response
+      """
+      {
+        "status": "OK",
+        "errors": [],
+        "tax_collected": 50.00,
+        "CAD": 66,928861615
+      }
+      """
 
   Scenario: Client perform a fund transfer without enough funds
 
