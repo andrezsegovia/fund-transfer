2a1643fec0012243026f094f8795296744cb6591
Andrez S <ajsegoviacruz93@gmail.com>
2021-05-29 22:35:40 -0500

    update account balance functionality

--- account-service/src/main/java/com/yellowpepper/accountservice/daos/AccountRepository.java
+++ account-service/src/main/java/com/yellowpepper/accountservice/daos/AccountRepository.java
@@ -2,15 +2,18 @@ package com.yellowpepper.accountservice.daos;
 
 import com.yellowpepper.accountservice.dtos.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public
-interface AccountRepository extends JpaRepository<Account, Integer> {
+@Transactional
+public interface AccountRepository extends JpaRepository<Account, Integer> {
 
     Account findAccountByAccount(String account);
 
+    @Modifying
     @Query("update Account a set a.accountBalance = ?2 where a.account = ?1")
     void updateAccountBalanceByAccount(String account, Float balance);
 }
